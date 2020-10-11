6ead819b61e963ac1dff785212c7913eedb0e5a5
Nandun Bandara <ntbandara3@hotmail.com>
2020-10-11 13:21:09 +0530

    Added order status to order model

---
+++ src/core/orders/order-status.js
@@ -0,0 +1,11 @@
+const ORDER_STATUS = {
+    PENDING: 'pending',
+    PICKEDUP: 'pickedup',
+    ONROUTE: 'onroute',
+    DELIVERED: 'delivered',
+    RETURNED: 'returned',
+}
+
+module.exports = {
+    ORDER_STATUS
+}
